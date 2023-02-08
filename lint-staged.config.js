37f280b0d72050167a447b1d7aba848c802da83a
Bhairesh <bhairesh@arithaconsulting.com>
2023-02-08 22:45:21 +0530

    update: lint-staged config added.

---
+++ lint-staged.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require("path");
+const { ESLint } = require("eslint");
+
+const eslint = new ESLint();
+
+module.exports = {
+  "**/*.{js,ts,tsx,json}": (fileNames) => {
+    const escapedFileNames = fileNames
+      .map((fileName) => escape(fileName))
+      // .map(fileName => (isWin ? fileName : escape([fileName]))) // * architecture check
+      .join(" ");
+
+    return [
+      `prettier --with-node-modules --write ${escapedFileNames}`,
+      `eslint --no-ignore --max-warnings=0 --fix ${fileNames
+        .filter((f) => !eslint.isPathIgnored(f))
+        .map((f) => `"${f}"`)
+        .join(" ")}`,
+      `git add ${escapedFileNames}`,
+    ];
+  },
+};
+
+function escape(str) {
+  const escaped = resolve(str);
+  return escaped;
+}
